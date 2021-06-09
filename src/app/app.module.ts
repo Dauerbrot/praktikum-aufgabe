3b93b54ea4fd804f0a6a4061cbb891e5bbf18abc
Dauerbrot <antrophob17@gmail.com>
2021-06-09 10:35:48 +0200

    1 file modified, 1 added, 1 deleted

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,13 @@ import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
-import { PatientmockService } from './patientmock.service';
+import { PatientmockService, PatientListService } from './patientmock.service';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent ],
-  bootstrap:    [ AppComponent ],
-  providers: [PatientmockService]
+  imports: [BrowserModule, FormsModule, HttpClientModule],
+  declarations: [AppComponent, HelloComponent],
+  bootstrap: [AppComponent],
+  providers: [{ provide: PatientListService, useClass: PatientmockService }]
 })
-export class AppModule { }
+export class AppModule {}
