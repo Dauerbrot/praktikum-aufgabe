ac5dc8e120b629ebe34cdc0d331a5926fc817f12
Dauerbrot <antrophob17@gmail.com>
2021-06-09 10:29:41 +0200

    2 files modified, 2 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,12 @@ import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
+import { PatientmockService } from './patientmock.service';
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule ],
   declarations: [ AppComponent, HelloComponent ],
-  bootstrap:    [ AppComponent ]
+  bootstrap:    [ AppComponent ],
+  providers: [PatientmockService]
 })
 export class AppModule { }
